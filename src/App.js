e58eaf4496c44853b93d152f37694f9ba751b54a
KhersonGirl <135836740+KhersonGirl@users.noreply.github.com>
2023-10-13 17:48:26 +0200

    Added css for all elements on the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,95 @@
-
-import './App.css';
-import SearchWeather from './SearchWeather';
+import "./App.css";
+import SearchWeather from "./SearchWeather";
+import ReactAnimatedWeather from "react-animated-weather";
 
 function App() {
   return (
     <div className="App container">
       <header className="App-header">
-      <SearchWeather />
+        <SearchWeather />
       </header>
+      <section className="App-section">
+        <div className="Weather-description row">
+          <div className="col-6">
+            <h2>Paris</h2>
+
+            <p>Thusday 12:00</p>
+            <p>Clear</p>
+          </div>
+          <div className="col-6">
+            <p>Humidity 20%</p>
+            <p>Pressure 1010pH</p>
+            <p>Wind 5 km/h</p>
+          </div>
+        </div>
+        <div className="Current-temperature d-inline-flex">
+          <ReactAnimatedWeather
+            icon={"CLEAR_DAY"}
+            color={"#0B5ED7"}
+            size={80}
+            animate={true}
+          />
+
+          <h1>23°C</h1>
+        </div>
+        <div className="Forcast">
+          <div className="row">
+            <div className="col-3">
+              <h3>Wed</h3>
+              <ReactAnimatedWeather
+                icon={"PARTLY_CLOUDY_DAY"}
+                color={"#0B5ED7"}
+                size={50}
+                animate={true}
+              />
+              <p>
+                <strong>20</strong>/14
+              </p>
+            </div>
+            <div className="col-3">
+              <h3>Thur</h3>
+              <ReactAnimatedWeather
+                icon={"CLEAR_DAY"}
+                color={"#0B5ED7"}
+                size={50}
+                animate={true}
+              />
+              <p>
+                <strong>22</strong>/16
+              </p>
+            </div>
+            <div className="col-3">
+              <h3>Fri</h3>
+              <ReactAnimatedWeather
+                icon={"CLOUDY"}
+                color={"#0B5ED7"}
+                size={50}
+                animate={true}
+              />
+              <p>
+                <strong>20</strong>/15
+              </p>
+            </div>
+            <div className="col-3">
+              <h3>Sat</h3>
+              <ReactAnimatedWeather
+                icon={"RAIN"}
+                color={"#0B5ED7"}
+                size={50}
+                animate={true}
+              />
+              <p>
+                <strong>18</strong>/13
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer>
+        <p>
+          <a href="">Open-source code</a>, by Nataliia Khanina
+        </p>
+      </footer>
     </div>
   );
 }
--- src/SearchWeather.js
+++ src/SearchWeather.js
@@ -1,90 +1,19 @@
 import React from "react";
 import "./App.css";
-import ReactAnimatedWeather from "react-animated-weather";
+
 
 export default function SearchWeather() {
   return (
-    <div className="container">
-      <form>
-        <input type="search" placeholder="Enter a city..." />
-        <button type="button" class="btn btn-primary">
+    <div className="Search-form ">
+      <form className="row">
+        <input type="search" placeholder="Enter a city..." className="col-6" />
+        <button type="button" className="btn btn-primary col-3">
           Search
         </button>
-        <button type="button" class="btn btn-success">
+        <button type="button" className="btn btn-success col-3">
           Current
         </button>
       </form>
-      <div className="row">
-        <div className="col-6">
-          <h2>Paris</h2>
-
-          <ul>
-            <li>Thusday 12:00</li>
-            <li>Clear</li>
-          </ul>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity 20%</li>
-            <li>Pressure 1010pH</li>
-            <li>Wind 5 km/h</li>
-          </ul>
-        </div>
-      </div>
-      <div>
-        <ReactAnimatedWeather
-          icon={"CLEAR_DAY"}
-          color={"#0B5ED7"}
-          size={50}
-          animate={true}
-        />
-
-        <h1>23°C</h1>
-      </div>
-      <div className="forcast">
-        <div className="row">
-          <div className="col-3">
-            <h3>Wed</h3>
-            <ReactAnimatedWeather
-              icon={"CLEAR_DAY"}
-              color={"#0B5ED7"}
-              size={50}
-              animate={true}
-            />
-            <p><strong>22</strong>/16</p>
-          </div>
-          <div className="col-3">
-            <h3>Thur</h3>
-            <ReactAnimatedWeather
-              icon={"CLEAR_DAY"}
-              color={"#0B5ED7"}
-              size={50}
-              animate={true}
-            />
-            <p><strong>22</strong>/16</p>
-          </div>
-          <div className="col-3">
-            <h3>Fri</h3>
-            <ReactAnimatedWeather
-              icon={"CLEAR_DAY"}
-              color={"#0B5ED7"}
-              size={50}
-              animate={true}
-            />
-            <p><strong>22</strong>/16</p>
-          </div>
-          <div className="col-3">
-            <h3>Sat</h3>
-            <ReactAnimatedWeather
-              icon={"CLEAR_DAY"}
-              color={"#0B5ED7"}
-              size={50}
-              animate={true}
-            />
-            <p><strong>22</strong>/16</p>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
