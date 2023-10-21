bb7b0ec5cb60e2a2d9e7ef39f7cca0be2682c4d6
KhersonGirl <135836740+KhersonGirl@users.noreply.github.com>
2023-10-21 21:26:42 +0200

    corrected css bei WeatherInfo

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,7 +17,7 @@ export default function WeatherInfo(props) {
           </p>
           <p className="text-capitalize">{props.data.description}</p>
         </div>
-        <div className="col-6 mt-5">
+        <div className="WeatherDescription col-6 mt-5">
           <p>
             Humidity: <span className="WeatherData">{props.data.humidity}</span>%
           </p>
@@ -34,82 +34,6 @@ export default function WeatherInfo(props) {
 
       <WeatherTemperature celsius={props.data.temperature} />
 
-      <div className="Weather-forcast">
-        <div className="row">
-          <div className="col-2">
-            <h3>Wed</h3>
-            <ReactAnimatedWeather
-              icon={"PARTLY_CLOUDY_DAY"}
-              color={"#0B5ED7"}
-              size={30}
-              animate={true}
-            />
-            <p>
-              <strong>20</strong>/14
-            </p>
-          </div>
-          <div className="col-2">
-            <h3>Thu</h3>
-            <ReactAnimatedWeather
-              icon={"CLEAR_DAY"}
-              color={"#0B5ED7"}
-              size={30}
-              animate={true}
-            />
-            <p>
-              <strong>22</strong>/16
-            </p>
-          </div>
-          <div className="col-2">
-            <h3>Fri</h3>
-            <ReactAnimatedWeather
-              icon={"CLOUDY"}
-              color={"#0B5ED7"}
-              size={30}
-              animate={true}
-            />
-            <p>
-              <strong>20</strong>/15
-            </p>
-          </div>
-          <div className="col-2">
-            <h3>Sat</h3>
-            <ReactAnimatedWeather
-              icon={"RAIN"}
-              color={"#0B5ED7"}
-              size={30}
-              animate={true}
-            />
-            <p>
-              <strong>18</strong>/13
-            </p>
-          </div>
-          <div className="col-2">
-            <h3>Sun</h3>
-            <ReactAnimatedWeather
-              icon={"RAIN"}
-              color={"#0B5ED7"}
-              size={30}
-              animate={true}
-            />
-            <p>
-              <strong>15</strong>/11
-            </p>
-          </div>
-          <div className="col-2">
-            <h3>Mo</h3>
-            <ReactAnimatedWeather
-              icon={"RAIN"}
-              color={"#0B5ED7"}
-              size={30}
-              animate={true}
-            />
-            <p>
-              <strong>17</strong>/10
-            </p>
-          </div>
-        </div>
-      </div> 
     </div>
   );
 }
